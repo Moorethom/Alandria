7232981f6b90feed0350f1aa74fb525014aef707
Thomas Moore <moore.thomas9911@gmail.com>
2018-03-11 05:35:37 +1300

    Added a heap of toggles to the hud class and can draw tabs for all the buildings and between types. It actually works quite well and should be fine minus the fact it is a lot more c and p than I would have liked. Is it spaget or not???
    https://www.youtube.com/watch?v=cE1FrqheQNI

--- src/main/java/com/processing/sketch/Hud.java
+++ src/main/java/com/processing/sketch/Hud.java
@@ -18,8 +18,27 @@ public class Hud {
 
     private Player player;
 
-    public boolean toggleBuildings;
     public boolean toggleResearch;
+    public boolean toggleBuildings;
+    //Building Tabs
+    public boolean toggleBProductionTab;
+    public boolean toggleBCivilianTab;
+    public boolean toggleBMilitaryTab;
+
+    //BuildingTypes
+    public boolean toggleHouse;  //c
+    public boolean toggleCapital;  //c
+    public boolean toggleBarracks;  //m
+    public boolean toggleFarm;  //p
+    public boolean toggleUniversity;  //c
+    public boolean toggleSchool; //c
+    public boolean toggleMarket; //c
+    public boolean toggleSmith; //p
+    public boolean toggleEngineeringBay; //p
+    public boolean toggleStable; //m
+    public boolean toggleTrainingGround; //m
+    public boolean toggleCityCenter; //c
+    public boolean toggleMine; //p
 
     int widthDi;
     int heightDi;
@@ -53,11 +72,27 @@ public class Hud {
         p.rect(0,0, widthDi, heightDi);
         p.rect(width/10, 0,widthDi, heightDi);
 
+        if(toggleBuildings){
+            drawBuildingsTab();
+            p.println("occur");
+        } else if(toggleResearch){
+            drawResearchTab();
+        } else if(toggleBMilitaryTab){
+            drawMilTab();
+        } else if(toggleBCivilianTab){
+            drawCivTab();
+        } else if(toggleBProductionTab){
+            drawProdTab();
+        }
+
+
         //Sets and the font and colors
         p.textFont(font);
-        p.fill(0);
         p.textSize(32);
 
+        p.pushMatrix();
+
+        p.fill(0);
         //Draws the text for the research and buildings boxes
         p.text("Buildings", 32, height /24);
         p.text("Research", 30+widthDi, height/24);
@@ -67,6 +102,8 @@ public class Hud {
         p.text("Wealth: " + convertFloat(wealthTemp), widthDi*3, (height / 24));
         p.text("Metal: " + convertFloat(metalTemp), widthDi*4, (height / 24));
         p.text("Pop: " + player.getPopulation(), widthDi*5, (height / 24));
+
+        p.popMatrix();
     }
 
     public void update() {
@@ -80,11 +117,157 @@ public class Hud {
     }
 
 
+    //Tab Drawings are here
+    public void drawBuildingsTab(){
+        p.fill(255);
+        p.rect(0,heightDi,widthDi,heightDi); //Could use a for loop here but it might slow down the code for no real need
+        p.rect(0,heightDi*2,widthDi,heightDi);
+        p.rect(0,heightDi*3,widthDi,heightDi);
+
+        p.fill(0);
+        p.text("Production",32,(heightDi)+46);
+        p.text("Civilian",32,(heightDi*2)+46);
+        p.text("Military",32,(heightDi*3)+46);
+
+    }
+
+    public void drawResearchTab(){
+
+    }
+
+    public void drawMilTab(){ //Miitary Tab Draws here
+        p.fill(255);
+        p.rect(0,heightDi,widthDi,heightDi);
+        p.rect(0,heightDi*2,widthDi,heightDi);
+        p.rect(0,heightDi*3,widthDi,heightDi);
+
+        p.fill(0);
+        p.text("Barracks",32,(heightDi)+46);
+        p.text("Stable",32,(heightDi*2)+46);
+        p.text("Training",32,(heightDi*3)+30);
+        p.text("Ground",32,(heightDi*3)+60);
+    }
+
+    public void drawCivTab(){ //Civilian Tab Draws here
+        p.rect(0,heightDi,widthDi,heightDi);
+        p.rect(0,heightDi*2,widthDi,heightDi);
+        p.rect(0,heightDi*3,widthDi,heightDi);
+        p.rect(0,heightDi*4,widthDi,heightDi);
+        p.rect(0,heightDi*5,widthDi,heightDi);
+        p.rect(0,heightDi*6,widthDi,heightDi);
+
+        p.fill(0);
+        p.text("House",32,(heightDi)+46);
+        p.text("Capital",32,(heightDi*2)+46);
+        p.text("University",32,(heightDi*3)+46);
+        p.text("School",32,(heightDi*4)+46);
+        p.text("Market",32,(heightDi*5)+46);
+        p.text("City",32,(heightDi*6)+30);
+        p.text("Center",32,(heightDi*6)+60);
+    }
+    public void drawProdTab(){ //Production Tab Draws here
+        p.fill(255);
+        p.rect(0,heightDi,widthDi,heightDi);
+        p.rect(0,heightDi*2,widthDi,heightDi);
+        p.rect(0,heightDi*3,widthDi,heightDi);
+        p.rect(0,heightDi*4,widthDi,heightDi);
+
+        p.fill(0);
+        p.text("Farm",32,(heightDi)+46);
+        p.text("Smith",32,(heightDi*2)+46);
+        p.text("Engineering",32,(heightDi*3)+30);
+        p.text("Bay",32,(heightDi*3)+60);
+        p.text("Mine",32,(heightDi*4)+46);
+    }
+
+
+
+    //Change Toggles are here
+    public boolean flipToggle(boolean toggle){
+        return toggle^=true;
+    }
+
+
+    //Hud
     public void toggleBuildings() {
-        toggleBuildings ^= true;
+        toggleBMilitaryTab = false;
+        toggleBCivilianTab = false;
+        toggleBProductionTab = false;
+
+        toggleBuildings = flipToggle(toggleBuildings);
     }
 
     public void toggleResearch() {
-        toggleResearch ^= true;
+        toggleResearch = flipToggle(toggleResearch);
     }
+
+    //Tabs
+    public void militaryTab() {
+        toggleBMilitaryTab = flipToggle(toggleBMilitaryTab);
+        toggleBuildings = flipToggle(toggleBuildings);
+    }
+    public void productionTab() {
+        toggleBProductionTab = flipToggle(toggleBProductionTab);
+        toggleBuildings = flipToggle(toggleBuildings);
+    }
+    public void civilianTab() {
+        toggleBCivilianTab = flipToggle(toggleBCivilianTab);
+        toggleBuildings = flipToggle(toggleBuildings);
+    }
+
+    //BuildingTypes
+    public void tHouse() {
+        toggleHouse = flipToggle(toggleHouse);
+        toggleBCivilianTab = flipToggle(toggleBCivilianTab);
+    }
+    public void tCapital() {
+        toggleCapital = flipToggle(toggleCapital);
+        toggleBCivilianTab = flipToggle(toggleBCivilianTab);
+    }
+    public void tBarracks() {
+        toggleBarracks = flipToggle(toggleBarracks);
+        toggleBMilitaryTab = flipToggle(toggleBMilitaryTab);
+    }
+    public void tFarm() {
+        toggleFarm = flipToggle(toggleFarm);
+        toggleBProductionTab = flipToggle(toggleBProductionTab);
+    }
+    public void tUniversity() {
+        toggleUniversity = flipToggle(toggleUniversity);
+        toggleBCivilianTab = flipToggle(toggleBCivilianTab);
+    }
+    public void tSchool() {
+        toggleSchool = flipToggle(toggleSchool);
+        toggleBCivilianTab = flipToggle(toggleBCivilianTab);
+    }
+    public void tMarket() {
+        toggleMarket = flipToggle(toggleMarket);
+        toggleBCivilianTab = flipToggle(toggleBCivilianTab);
+    }
+    public void tSmith() {
+        toggleSmith = flipToggle(toggleSmith);
+        toggleBProductionTab = flipToggle(toggleBProductionTab);
+    }
+    public void tEngineeringBay() {
+        toggleEngineeringBay = flipToggle(toggleEngineeringBay);
+        toggleBProductionTab = flipToggle(toggleBProductionTab);
+    }
+    public void tStable() {
+        toggleStable = flipToggle(toggleStable);
+        toggleBMilitaryTab = flipToggle(toggleBMilitaryTab);
+    }
+    public void tTrainingGround() {
+        toggleTrainingGround = flipToggle(toggleTrainingGround);
+        toggleBMilitaryTab = flipToggle(toggleBMilitaryTab);
+    }
+    public void tCityCenter() {
+        toggleCityCenter = flipToggle(toggleCityCenter);
+        toggleBCivilianTab = flipToggle(toggleBCivilianTab);
+    }
+    public void tMine() {
+        toggleMine = flipToggle(toggleMine);
+        toggleBProductionTab = flipToggle(toggleBProductionTab);
+    }
+
+
 }
--- src/main/java/com/processing/sketch/Mouse.java
+++ src/main/java/com/processing/sketch/Mouse.java
@@ -12,6 +12,9 @@ public class Mouse {
     private int mouseY;
     private int mouseX;
 
+    private int heightDi;
+    private int widthDi;
+
     public Mouse(PApplet p) {
         this.p = p;
     }
@@ -26,16 +29,69 @@ public class Mouse {
         this.mouseX = mouseXPos;
         this.mouseY = mouseYPos;
 
-        if (mouseY <= heightTemp/16){ //if the mouse is clicked in the hud
+        heightDi = heightTemp/16; //Commonly Used height variable
+        widthDi = widthTemp/10; //Commonly Used Width variable
 
-            if (mouseX > 0 && mouseX <= widthTemp/10) { //first tool
+        if (mouseY <= heightTemp / 16) { //if the mouse is clicked in the Top hud
+
+            if (mouseX > 0 && mouseX <= widthDi) {  //This is for checking when no tabs are open
                 hudTemp.toggleBuildings(); //Turns the display on/off for the Buildings box
                 //p.println("Buildings"); //Testing
 
-            } else if (mouseX > widthTemp/10 && mouseX <= (widthTemp/10)*2){ //2nd tool
+            } else if (mouseX > widthDi && mouseX <= widthDi * 2) {
                 hudTemp.toggleResearch(); //Turns the display on/off for the Research box
                 //p.println("Research"); //Testing
             }
         }
+
+        if (mouseX > 0 && mouseX <= widthDi) { //This is for an open building Tab
+            if(hudTemp.toggleBuildings){ //Checks if the building Hud is open
+
+                if(mouseY > heightDi && mouseY <= heightDi*2){ //Production tab check
+                    hudTemp.productionTab();
+                } else if (mouseY > heightDi*2 && mouseY <= heightDi*3) {
+                    hudTemp.civilianTab();
+                } else if (mouseY > heightDi*3 && mouseY <= heightDi*4) {
+                    hudTemp.militaryTab();
+                }
+
+            } else if (hudTemp.toggleBMilitaryTab){ //Military Tab is open
+                if(mouseY > heightDi && mouseY <= heightDi*2){
+                    hudTemp.tBarracks();
+                } else if (mouseY > heightDi*2 && mouseY <= heightDi*3) {
+                    hudTemp.tStable();
+                } else if (mouseY > heightDi*3 && mouseY <= heightDi*4) {
+                    hudTemp.tTrainingGround();
+                }
+
+            } else if (hudTemp.toggleBCivilianTab){ //Civilian Tab is open
+                if(mouseY > heightDi && mouseY <= heightDi*2){
+                    hudTemp.tHouse();
+                } else if (mouseY > heightDi*2 && mouseY <= heightDi*3) {
+                    hudTemp.tCapital();
+                } else if (mouseY > heightDi*3 && mouseY <= heightDi*4) {
+                    hudTemp.tUniversity();
+                } else if (mouseY > heightDi*4 && mouseY <= heightDi*5) {
+                    hudTemp.tSchool();
+                } else if (mouseY > heightDi*5 && mouseY <= heightDi*6) {
+                    hudTemp.tMarket();
+                } else if (mouseY > heightDi*6 && mouseY <= heightDi*7) {
+                    hudTemp.tCityCenter();
+                }
+
+            } else if (hudTemp.toggleBProductionTab){ //Production Tab is open
+                if(mouseY > heightDi && mouseY <= heightDi*2){
+                    hudTemp.tFarm();
+                } else if (mouseY > heightDi*2 && mouseY <= heightDi*3) {
+                    hudTemp.tSmith();
+                } else if (mouseY > heightDi*3 && mouseY <= heightDi*4) {
+                    hudTemp.tEngineeringBay();
+                } else if (mouseY > heightDi*4 && mouseY <= heightDi*5) {
+                    hudTemp.tMine();
+                }
+
+            }
+        }
+
     }
 }
